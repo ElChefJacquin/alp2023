653b154efa84320a7bb8f279a08d028769e1cfab
ElChefJacquin <capar.2022113isinf0305@aluno.ifpi.edu.br>
2023-05-02 00:30:23 -0300

    Mudanças Finais

--- script.js
+++ script.js
@@ -8,13 +8,10 @@ const questao7 = document.getElementById("0507");
 const questao8 = document.getElementById("0508");
 
 questao1.addEventListener("click", function () {
-var v1 = 8 + 9 + 7;
-var v2 = v1 / 3;
-var v3 = 4 + 5 + 6;
-var v4 = v3 / 3;
-var v5 = v2 + v4;
-var v6 = v5 / 2;
-alert('A média de 8, 9 e 7 é ' + v2 + '\nA média de 4, 5 e 6 é ' + v4 + '\nA soma das duas médias é ' + v5 + '\nA média das médias é ' + v6 + '.');
+var v1 = (8+9+7)/3;
+var v2 = (5+6+7)/3;
+var v3 = (v2+v1)/2;
+alert(`A média final das notas é de ${v3}`)
 });
 
 questao2.addEventListener("click", function () {
@@ -23,54 +20,55 @@ var quant1 = Number(prompt('Insira a quantidade:'));
 var valor2 = Number(prompt('Insira o segundo valor:'));
 var quant2 = Number(prompt('Insira a quantidade:'));
 var ipi = Number(prompt('Insira o imposto (sem porcentagem):'));
-var cal = (valor1 * quant1 + valor2 * quant2) * (ipi / 100 + 1);
-alert('O valor com o imposto fica ' + cal.toFixed(2) + '.');
-})
+var cal = (valor1* quant1 + valor2 * quant2) * (ipi / 100 + 1);
+alert(`O valor com o imposto fica ${cal.toFixed(2)}`);
+});
 
 questao3.addEventListener("click", function () {
 var salarioa = Number(prompt('Insira o salário mínimo atual:'));
 var salariob = Number(prompt('Insira o seu salário atual:'));
 var s = salariob / salarioa;
-alert('Você ganha aproximadamente ' + s.toFixed(1) + '.');
-})
+alert(`Você ganha aproximadamente ${s.toFixed(2)} salários mínimos`);
+});
+
 questao4.addEventListener("click", function () {
 var numero = Number(prompt('Insira um número inteiro:'));
 var sucessor = numero + 1;
 var antecessor = sucessor - 2;
-alert('Seu antecessor é ' + antecessor + ' e o sucessor é ' + sucessor + '.');
+alert(`Seu antecessor é ${antecessor} e o sucessor é ${sucessor}.`);
 });
+
 questao5.addEventListener("click", function () {
 var va1 = Number(prompt('Insira o valor de A:'));
 var va2 = Number(prompt('Insira o valor de B:'));
-var troca1 = va2;
-va2 = va1;
-va1 = troca1;
-alert('Valor trocado de A é ' + va1 + ' e de B é ' + va2 + '.');
+alert(`Valor trocado de A é ${va2} e de B é ${va1}`);
 });
+
 questao6.addEventListener("click", function () {
 var valor = Number(prompt('Insira o valor do seu produto adquirido aqui na loja mamão com açúcar:'));
-var pre1 = valor / 1;
 var pre2 = valor / 2;
 var pre3 = valor / 3;
 var pre4 = valor / 4;
 var pre5 = valor / 5;
-alert('Você tem parcelas de 1x de $' + pre1.toFixed(2) + '\n2x de $' + pre2.toFixed(2) + '\n3x de $' + pre3.toFixed(2) + '\n4x de $' + pre4.toFixed(2) + '\n5x de $' + pre5.toFixed(2) + '.');
+alert(`Parcelas disponíveis para ${valor.toFixed(2)}:\n1x de ${valor.toFixed(2)}\n2x de ${pre2.toFixed(2)}\n3x de ${pre3.toFixed(2)}\n4x de ${pre4.toFixed(2)}\n5x de ${pre5.toFixed(2)}`);
 });
+
 questao7.addEventListener("click", function () {
 var custo = Number(prompt('Insira o custo:'));
 var percentual = Number(prompt('Insira o percentual:'));
-var calpercentual = custo * (percentual / 100);
-var vf = calpercentual + custo;
-alert('O valor de venda é $' + vf.toFixed(2) + '.');
+var cal = custo * (percentual / 100);
+var vf = cal + custo;
+alert(`O valor de venda é $${vf}`);
 });
+
 questao8.addEventListener("click", function () {
 let num1 = Number(prompt("Insira o primeiro valor:"));
 let num2 = Number(prompt("Insira o segundo valor:"));
 
-let soma = num1 + num2;
-let sub = num1 - num2;
-let multi = num1 * num2;
-let div = num1 / num2;
+let soma = num1+num2;
+let sub = num1-num2;
+let multi = num1*num2;
+let div = num1/num2;
 
 alert(`Soma = ${soma.toFixed(2)}\n Subtração = ${sub.toFixed(2)}\n Multiplicação = ${multi.toFixed(2)}\n Divisão = ${div.toFixed(2)}`);
 });
